b19ff8c5604eb5686d31860836759d08698bf74d
har4s <khalvatihares@gmail.com>
2022-06-07 00:59:20 +0430

    build

--- lib/useSessionStorage.d.ts
+++ lib/useSessionStorage.d.ts
@@ -2,4 +2,4 @@ import { SetStateAction } from "react";
 import { UseLocalStorage, UseLocalStorageOptions } from "./useLocalStorage";
 export interface UseSessionStorageOptions extends Omit<UseLocalStorageOptions, "persistent"> {
 }
-export default function useSessionStorage<T>(key: string, initialValue: SetStateAction<T>, options?: UseLocalStorageOptions): UseLocalStorage<T>;
+export default function useSessionStorage<T>(key: string, initialValue: SetStateAction<T>, options?: UseSessionStorageOptions): UseLocalStorage<T>;
