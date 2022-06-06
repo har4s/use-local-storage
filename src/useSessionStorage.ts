2cffc2341bb4459f7dfa5a2e7ba1966cf1fa631d
har4s <khalvatihares@gmail.com>
2022-06-06 23:53:28 +0430

    use base hook

--- src/useSessionStorage.ts
+++ src/useSessionStorage.ts
@@ -4,12 +4,13 @@ import useLocalStorage, {
   UseLocalStorageOptions,
 } from "./useLocalStorage";
 
-export interface UseSessionStorageOptions extends UseLocalStorageOptions {}
+export interface UseSessionStorageOptions
+  extends Omit<UseLocalStorageOptions, "persistent"> {}
 
 export default function useSessionStorage<T>(
   key: string,
   initialValue: SetStateAction<T>,
   options?: UseLocalStorageOptions
 ): UseLocalStorage<T> {
-  return useLocalStorage(key, initialValue, options);
+  return useLocalStorage(key, initialValue, { ...options, persistent: false });
 }
